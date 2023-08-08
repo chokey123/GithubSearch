db20bc4d3f9f6fd0971f65dd94dffd90efb4fb26
WONG YI XING <TP068170@mail.apu.edu.my>
2023-08-08 14:09:26 +0800

    initial commit

---
+++ src/components/layout/Ayaka.jsx
@@ -0,0 +1,15 @@
+import ayaka from "./assets/image.png";
+
+function Ayaka() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        width={180}
+        src={ayaka}
+        alt="ayaka..."
+        className="text-center mx-auto"
+      />
+    </div>
+  );
+}
+export default Ayaka;
